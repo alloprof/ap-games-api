2c463e57cc0062ec7ff5ca616fc7cd9f26f859e3
Guillaume Allo <guillaume.mbali.dev@gmail.com>
2025-12-18 15:09:54 -0500

    fix: squidex export conf

--- src/squidex/index.ts
+++ src/squidex/index.ts
@@ -1,4 +1,4 @@
 export { squidexRouter } from './routes'
 export { SquidexClient } from './client'
 export * from './types'
-export type { SquidexAppConfig } from '../core/config/config'
+export type { SquidexAppConfig } from '../core/config/helpers'
